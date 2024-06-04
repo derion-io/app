d70ea70b7c62ccf1ae1ed4fa9bf10f7d1a45b213
Zergity <zergity@gmail.com>
2024-06-04 16:14:27 +0700

    override process/browser.js

--- config-overrides.js
+++ config-overrides.js
@@ -14,7 +14,10 @@ module.exports = function override(config) {
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
       Buffer: ['buffer', 'Buffer']
-    })
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser.js'
+    }),
   ])
   return config;
 }
